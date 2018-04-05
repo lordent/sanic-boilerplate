f9605475d231595a70276e9849b5ab937aebadff
Nefyodov Vitaliy <n.vitaliy@everyads.com>
2018-04-05 19:13:25 +0300

    tests

---
+++ oad/tests/test_openapi.py
@@ -0,0 +1,54 @@
+import pytest
+from openapi_spec_validator import validate_spec
+from sanic import Sanic, response
+from sanic.views import HTTPMethodView
+from sanic.request import Request
+
+from oad import openapi
+
+
+@pytest.yield_fixture
+def fix_app(fix_doc_settings):
+    app = Sanic('test_sanic_app', strict_slashes=True)
+
+    @openapi.doc({
+        'summary': 'Test summary text',
+        'description': 'Test description',
+    })
+    @openapi.request()
+    @openapi.response()
+    @app.route('/test-parameter-int/<parameter:int>')
+    async def test_handler(request: Request, parameter: int):
+        return response.json({
+            'parameter': parameter,
+        })
+
+    class TestHTTPMethodView(HTTPMethodView):
+        @openapi.request({
+            'summary': 'Test summary text',
+            'description': 'Test description',
+        }, content_documentation={
+            'example': {
+                'name': 'Fluffy',
+                'petType': 'dog',
+            },
+        }, schema={
+            'type': 'object',
+            'properties': {
+                'id': {
+                    'type': 'integer',
+                    'format': 'int64',
+                    'example': '4',
+                },
+            },
+        })
+        @openapi.response()
+        async def post(self, request, parameter: str):
+            return response.json({
+                'parameter': parameter,
+                'success': True
+            })
+
+    app.add_route(TestHTTPMethodView.as_view(), '/test-view-doc')
+
+    yield app
